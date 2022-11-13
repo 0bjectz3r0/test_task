e513fefa89a45db3ea39e84a63ee250652d27e0e
0bjectz3r0 <118129913+0bjectz3r0@users.noreply.github.com>
2022-11-13 22:13:31 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -1,5 +1,10 @@
 <?php
-
+	
+	$servername = "localhost";
+	$username = "username";
+	$password = "password";
+	$connetion = mysqli_connect($servername, $username, $password);
+	
 	$nameError = $emailError = "";
 	$name = $_POST["name"];
 	$email = $_POST["email"];
@@ -24,6 +29,21 @@
 			}
 		}		
 	}
+	
+	if($connection === false){
+		die("ОШИБКА:" . mysqli_connect_error());
+	}
+	
+	$fullName = mysqli_real_escape_string($connection, $_REQUEST["name"]);
+	$userEmail = mysqli_real_escape_string($connection, $_REQUEST["email"]);
+	
+	$sql = "INSERT INTO messages (full_name, email) VALUES ('$fullName', '$userEmail')";
+	if(mysqli_query($connection, $sql)) {
+		echo "Записи добавлены";
+	} else {
+		echo "ОШИБКА: " . mysqli_error($connection);
+	}
+	mysqli_close($connetion);
 	
 	funtion inputValidate($data) {
 		$data = trim($data);
